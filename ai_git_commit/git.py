d2b3d8e9460d5e92bb8c830552a924cf72898d30
Raju Ghorai <amirajubolchi001@gmail.com>
2023-03-17 17:27:44 +0530

    ♻️ Code Refactoring: changed `git.py` code formant
    ### Description
     - added new python package `pygments_markdown_lexers` for applying markdown format
     - rename few variables in `git.py` file

--- ai_git_commit/git.py
+++ ai_git_commit/git.py
@@ -1,84 +1,110 @@
-import os
 import subprocess
-from typing import Dict, List, Optional
 
-from prompt_toolkit import prompt
-from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit import HTML, PromptSession, print_formatted_text
+from prompt_toolkit.completion import Completion, WordCompleter
+from prompt_toolkit.lexers import PygmentsLexer
+from prompt_toolkit.styles import Style
+from pygments.lexers import HtmlLexer
+from pygments_markdown_lexer.lexer import MarkdownLexer
 
 from ai_git_commit.config import ICommitMessage
 
-DEFAULT_TYPE_OPTIONS = [
-    {"value": "feat", "label": "✨ feat", "hint": "A new feature"},
-    {"value": "fix", "label": "🐛 fix", "hint": "A bug fix"},
-    {"value": "docs", "label": "📝 docs", "hint": "Documentation only changes"},
-    {
-        "value": "refactor",
-        "label": "🔨 refactor",
-        "hint": "A code change that neither fixes a bug nor adds a feature",
-    },
-    {
-        "value": "perf",
-        "label": "🚀 perf",
-        "hint": "A code change that improves performance",
-    },
-    {
-        "value": "test",
-        "label": "🧪 test",
-        "hint": "Adding missing tests or correcting existing tests",
-    },
-    {
-        "value": "build",
-        "label": "🏗️ build",
-        "hint": "Changes that affect the build system or external dependencies",
-    },
-    {
-        "value": "ci",
-        "label": "🤖 CI",
-        "hint": "Changes to our CI configuration files and scripts",
-    },
-]
-
-
-def get_git_diff_output() -> str:
-    result = subprocess.run(["git", "diff", "--staged"], capture_output=True, text=True)
-    if result.returncode != 0:
-        raise subprocess.CalledProcessError(result.returncode, result.stderr)
-    return result.stdout
-
-
-def get_git_status_short_output() -> str:
-    result = subprocess.run(
-        ["git", "status", "--short"], capture_output=True, text=True
-    )
-    if result.returncode != 0:
-        raise subprocess.CalledProcessError(result.returncode, result.stderr)
-    return result.stdout
-
 
 def git_user_commit_message() -> ICommitMessage:
-    type_completer = WordCompleter([option["value"] for option in DEFAULT_TYPE_OPTIONS])
-    selected_type = prompt(
-        f"Select the commit type ({','.join(list(type_completer.words))}): ",
-        completer=type_completer,
-        complete_while_typing=True,
+    style = Style.from_dict(
+        {
+            "completion-menu.completion": "bg:#008888 #ffffff",
+            "completion-menu.completion.current": "bg:#00aaaa #000000",
+            "scrollbar.background": "bg:#88aaaa",
+            "scrollbar.button": "bg:#222222",
+        }
+    )
+
+    type_options = [
+        {"value": "feat", "label": "✨ Features", "hint": "A new feature"},
+        {"value": "fix", "label": "🐞 Bug Fixes", "hint": "A bug fix"},
+        {
+            "value": "docs",
+            "label": "📝 Documentation",
+            "hint": "Documentation only changes",
+        },
+        {
+            "value": "refactor",
+            "label": "♻️ Code Refactoring",
+            "hint": "A code change that neither fixes a bug nor adds a feature",
+        },
+        {
+            "value": "perf",
+            "label": "🚀 Performance",
+            "hint": "A code change that improves performance",
+        },
+        {
+            "value": "test",
+            "label": "🧪 Tests",
+            "hint": "Adding missing tests or correcting existing tests",
+        },
+        {
+            "value": "build",
+            "label": "🏗️ Build",
+            "hint": "Changes that affect the build system or external dependencies",
+        },
+        {
+            "value": "ci",
+            "label": "🤖 CI",
+            "hint": "Changes to our CI configuration files and scripts",
+        },
+        {
+            "value": "style",
+            "label": "💄 Styles",
+            "hint": "A code change that improves code styles",
+        },
+    ]
+
+    type_completer = WordCompleter([option["value"] for option in type_options])
+    prompt_session = PromptSession(
+        completer=type_completer, style=style, lexer=PygmentsLexer(MarkdownLexer)
+    )
+
+    def get_completions(document, complete_event):
+        word_before_cursor = document.get_word_before_cursor()
+        for option in type_options:
+            if option["value"].startswith(word_before_cursor):
+                yield Completion(
+                    option["value"],
+                    start_position=-len(word_before_cursor),
+                    display=option["value"],
+                    display_meta=option["hint"],
+                )
+
+    prompt_session.completer.get_completions = get_completions
+
+    selected_type = prompt_session.prompt(
+        HTML(
+            "<b>Select the commit type <style fg='ansiwhite' bg='#00ff44'>[TAB]</style>:</b> "
+        )
     )
 
     commit_type = next(
         (
             option["label"]
-            for option in DEFAULT_TYPE_OPTIONS
+            for option in type_options
             if option["value"] == selected_type
         ),
         None,
     )
-    commit_subject = input(
-        f"{commit_type}: "
+
+    commit_subject = prompt_session.prompt(
+        HTML(f"<style fg='ansiwhite' bg='#00ff44'>{commit_type}:</style> ")
         if commit_type
         else "Write a brief title description for commit: "
     )
+
     commit_messages = []
+    print_formatted_text(
+        HTML("<ansiblue>Description [ENTER to exit or skip]</ansiblue>")
+    )
     while True:
-        msg = input(" - ")
+        msg = prompt_session.prompt(" - ")
         if not msg:
             break
         commit_messages.append(msg)
@@ -89,9 +115,48 @@ def git_user_commit_message() -> ICommitMessage:
     return ICommitMessage(id=0, subject=commit_subject, body=commit_messages)
 
 
-# def isInitGitRepository() -> bool:
-#     pass
+def get_git_diff_output() -> str:
+    result = subprocess.run(["git", "diff", "--staged"], capture_output=True, text=True)
+    if result.returncode != 0:
+        raise subprocess.CalledProcessError(result.returncode, result.stderr)
+    return result.stdout
 
 
-# def execGitCommit(commitMessage: Dict[str, Any]) -> str:
-#     pass
+def is_init_git_repository() -> bool:
+    try:
+        subprocess.check_output(["git", "rev-parse", "--is-inside-work-tree"])
+        return True
+    except:
+        return False
+
+
+def exec_git_commit(commitMessage: ICommitMessage) -> None:
+    with open("./.git/COMMIT_EDITMSG", "w") as f:
+        f.write(f"{commitMessage['subject']}\n\n")
+        f.write("### Description\n")
+        for i in commitMessage["body"]:
+            f.write(f" - {i}\n")
+
+    subprocess.check_output(["git", "commit", "-F", "./.git/COMMIT_EDITMSG"])
+
+
+def get_git_status_short_output() -> str:
+    result = subprocess.run(
+        ["git", "status", "--short", "--untracked-files=no"],
+        capture_output=True,
+        text=True,
+    )
+    if result.returncode != 0:
+        raise subprocess.CalledProcessError(result.returncode, result.stderr)
+    return result.stdout
+
+
+def get_hint(option_value):
+    return next(
+        (
+            option["hint"]
+            for option in type_options
+            if type_options["value"] == option_value
+        ),
+        None,
+    )
--- ai_git_commit/openai.py
+++ ai_git_commit/openai.py
@@ -8,24 +8,24 @@ from ai_git_commit.prompts import prompts
 openai.api_key = ""
 
 
-# async def generate_commit_messages(
-#     diff: str,
-#     num_of_commit_messages: int = 1,
-#     model: str = "text-davinci-002",
-#     max_tokens: int = 1024,
-#     max_retries: int = 3,
-#     retry_delay: int = 500,
-# ):
-#     if len(diff) == 0:
-#         raise ValueError("No diff provided")
+def generate_commit_messages(
+    diff: str,
+    num_of_commit_messages: int = 1,
+    model: str = "text-davinci-002",
+    max_tokens: int = 1024,
+    max_retries: int = 3,
+    retry_delay: int = 500,
+):
+    if len(diff) == 0:
+        raise ValueError("No diff provided")
 
-#     selected_prompt = prompts[5]
-#     prompt = selected_prompt["prompt"](
-#         diff=diff, num_of_commit_messages=num_of_commit_messages
-#     )
+    selected_prompt = prompts[5]
+    prompt = selected_prompt["prompt"](
+        diff=diff, num_of_commit_messages=num_of_commit_messages
+    )
 
-#     for current_retry in range(max_retries, 0, -1):
-#         response = openai.Completion.create(
-#             engine=model, prompt=prompt, max_tokens=max_tokens, temperature=0.35
-#         )
-#         return response
+    for current_retry in range(max_retries, 0, -1):
+        response = openai.Completion.create(
+            engine=model, prompt=prompt, max_tokens=max_tokens, temperature=0.35
+        )
+        return response
